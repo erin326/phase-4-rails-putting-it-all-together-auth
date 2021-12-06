a7342ad57528985525dc573e88ec0b0da887698e
erin <erinverkoulen93@gmail.com>
2021-12-06 17:02:54 -0600

    commit tree 54677f6610dac81c3ac8bf3a4aa3524e80dcd2db

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,34 @@
+class RecipesController < ApplicationController
+
+    def index 
+        user = User.find_by(id: session[:user_id])
+    
+        if user
+            render json: user.recipes, include: :user 
+        else
+            render json: {errors: ['Not authorized']}, status: :unauthorized
+        end
+    end
+
+    def create 
+        user = User.find_by(id: session[:user_id])
+
+        if user 
+            recipe = Recipe.create(recipe_params)
+            user.recipes << recipe 
+            if recipe.valid?
+                render json: recipe, include: :user, status: :created
+            else render json: {errors: recipe.errors.full_messages}, status: :unprocessable_entity
+            end
+        else
+            render json: {errors: ['Not authorized']}, status: :unauthorized 
+        end
+    end
+    
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.create(title: "Mac and Cheese", instructions: "Boil macaroni, add cheeses to a pot with 1 cup of cream, stir until you have a cheese roux. Place cooked macaroni in baking dish, pour over cheese mixture, and add cheese to top. Bake at 400 degrees for 20 minutes.", minutes_to_complete: 35)
+
+User.create(username: 'erin93', image_url:'https://www.pexels.com/photo/green-and-blue-peacock-feather-674010/', bio: "I'm a 28 year old woman from Evanston, Illinois. I'm studying to be a software engineer at Flatiron School." )
